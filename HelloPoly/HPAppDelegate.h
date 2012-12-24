9a4fc51a1d25888f3974c3e465c2b0ecaac9ed21
juanrgar <juanrgar@gmail.com>
2012-12-24 10:00:05 +0100

    Initial Commit

---
+++ HelloPoly/HPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HPAppDelegate.h
+//  HelloPoly
+//
+//  Created by Juan Rafael García Blanco on 12/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HPViewController;
+
+@interface HPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HPViewController *viewController;
+
+@end
---
+++ HelloPoly/HPViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPViewController.h
+//  HelloPoly
+//
+//  Created by Juan Rafael García Blanco on 12/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPViewController : UIViewController
+
+@end
